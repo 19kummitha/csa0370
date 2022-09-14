8fa79feaad6a4d834af920859f99ceba049f5af3
19kummitha <113335352+19kummitha@users.noreply.github.com>
2022-09-13 22:38:55 -0700

    Create factorial(recursion).c

---
+++ factorial(recursion).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int facto(n)
+{
+    if(n==1)
+        return 1;
+    else
+        return(n*facto(n-1));
+}
+void main()
+{
+    int n,fact;
+    printf("enter the number");
+    scanf("%d",&n);
+    fact=facto(n);
+    printf("factorial of number is%d",fact);
+}
